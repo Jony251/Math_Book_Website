89c16da7bdf035cade99d158d661340f64c252b0
Jont251 <nevgeny90@gmail.com>
2025-02-13 10:33:54 +0200

    Update: contact form styling, favicon, and social links

--- src/components/buttons/call_btn.vue
+++ src/components/buttons/call_btn.vue
@@ -1,7 +1,7 @@
 <template>
   <a href="tel:+972558858231" class="call-btn">
     <img src="/images/phone-icon.png" alt="Phone">
-    <span>+972 55-885-8231</span>
+    <span>Связаться с нами</span>
   </a>
 </template>
 
--- src/components/footer/footer.vue
+++ src/components/footer/footer.vue
@@ -7,17 +7,15 @@
       <div class="footer-section">
         <h3>Контакты</h3>
         <p><i class="fas fa-phone"></i> +972 55-885-8231</p>
-        <p><i class="fas fa-envelope"></i> info@mathbook.com</p>
+        <p><i class="fas fa-envelope"></i> dalevitan17@gmail.com</p>
       </div>
 
       <div class="footer-section">
         <h3>Социальные сети</h3>
         <div class="social-links">
-          <!--TODO: update the links of the social networks-->
           <a href="https://www.facebook.com/daria.jenner.5" class="social-link"><img src="/images/facebook_icon.png" alt="facebook"></a>
-          <a href="#" class="social-link"><img src="/images/instagram_icon.png" alt="instagram"></a>
-          <a href="#" class="social-link"><img src="/images/telegram_icon.png" alt="telegram"></a>
-          <a href="#" class="social-link"><img src="/images/whatsapp_icon.png" alt="whatsapp"></a>
+          <a href="https://t.me/levitanddd" class="social-link"><img src="/images/telegram_icon.png" alt="telegram"></a>
+          <a href="https://wa.me/+972558858231" class="social-link"><img src="/images/whatsapp_icon.png" alt="whatsapp"></a>
         </div>
       </div>
 
